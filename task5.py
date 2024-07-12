8f86ce42be061cb89c245398015882708a17d6a8
AnupChole <167945231+AnupChole@users.noreply.github.com>
2024-07-12 20:19:39 +0530

    Create task5.py

---
+++ task5.py
@@ -0,0 +1,9 @@
+def pattern(n):
+    for i in range(1,n+1):
+        for j in range(n-i):
+            print(" ",end="")
+        for k in range(1,2*i):
+            print('*',end="")
+        print()
+        
+pattern(9)
